b8b899265a5574abd2c19d8857039309184a357c
s-ajaykumar <your-email@example.com>
2025-06-28 23:52:00 +0530

    idea2_module2 worked and tested but the results are not expected

--- src/idea2_module2.py
+++ src/idea2_module2.py
@@ -13,44 +13,72 @@ torch.manual_seed(1337)
 
 @dataclass
 class Config:
-    batch_size = 8
+    batch_size = 4
     block_size = 20
-    core_block_size = 4
+    content_block_size = 4
     vocab_size = None
+    stop_emb_size = None
     stop_vocab_size = None
-    n_embd = 128
-    n_layers = 4
-    n_heads = 4
-    max_iters = 10000
+    n_embd = 32
+    n_layers = 2
+    n_heads = 2
+    max_iters = 1000
     eval_iters = 200
-    eval_interval = 1000
+    eval_interval = 100
     lr = 1e-3
     dropout_ratio = 0.2
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    mask_token = 171
+    x_mask_token = 3377
+    y_mask_token = 0
+    x_pad_token = 3549
+    y_pad_token = -1
 config = Config()
 
 
 
+def pad(x, y):
+    x_padded = []
+    y_padded = []
+    max_length = 0
+
+    for sample in x:
+        if len(sample) > max_length:
+            max_length = len(sample)
+
+    for x_sample, y_sample in zip(x, y):
+        diff = max_length - len(x_sample)
+        x_pad = torch.full((diff, ), config.x_pad_token, dtype = torch.long)
+        y_pad = torch.full((diff, ), config.y_pad_token, dtype = torch.long)
+        x_sample = torch.cat((x_sample, x_pad))
+        y_sample = torch.cat((y_sample, y_pad))
+        x_padded.append(x_sample)
+        y_padded.append(y_sample)
+
+    return torch.stack(x_padded), torch.stack(y_padded)
+
+
 
 def get_batch(mode):
+    xy_data = encoded_xy
+    yy_data = encoded_yy
+    
     if mode == 'train':
         data = train_data
-        y_data = y_train_data
+        ixs = train_ixs
     else:
         data = val_data
-        y_data = y_val_data
+        ixs = val_ixs
         
-    ixs = [random.randint(0, len(data)-1-config.core_block_size) for _ in range(config.batch_size)]   
     xb = []
-    yb = []   
-    for ix in ixs:    
-        x = torch.cat(data[ix:ix+config.core_block_size], y_data[x_ixs[ix]:x_ixs[ix+config.core_block_size-1]])
-        y = torch.cat(data[ix+1:ix+config.core_block_size], y_data[x_ixs[ix]:x_ixs[ix+config.core_block_size-1]+1])
+    yb = []
+    batch_ixs = [random.randint(0, len(data)-config.content_block_size) for _ in range(config.batch_size)]   
+       
+    for ix in batch_ixs:    
+        x = torch.cat((data[ix:ix+config.content_block_size], xy_data[ixs[ix]:ixs[ix+config.content_block_size-1]]))
+        y = torch.cat((torch.full((config.content_block_size-1, ), config.y_pad_token, dtype = torch.long), yy_data[ixs[ix]:ixs[ix+config.content_block_size-1]+1]))
         xb.append(x)
         yb.append(y)
-    xb = pad(x)
-    yb = pad(y)
+    xb, yb = pad(xb, yb)
     xb, yb = xb.to(config.device), yb.to(config.device)
     return xb, yb
 
@@ -76,7 +104,6 @@ class CausalSelfAttention(nn.Module):
         super().__init__()
         self.c_attn = nn.Linear(config.n_embd, 3*config.n_embd, bias = False)
         self.c_proj = nn.Linear(config.n_embd, config.n_embd, bias = False)
-        self.register_buffer('bias', torch.tril(torch.ones(config.block_size, config.block_size)).view(1, 1, config.block_size, config.block_size))
         self.c_proj.res_flag = 1
         
     def forward(self, x):
@@ -125,13 +152,12 @@ class GPT(nn.Module):
     def __init__(self):
         super().__init__()
         self.transformer = nn.ModuleDict(dict(
-            wte = nn.Embedding(config.vocab_size, config.n_embd),
-            wpe = nn.Embedding(config.block_size, config.n_embd),
+            wte = nn.Embedding(config.stop_emb_size, config.n_embd),
+            wpe = nn.Embedding(50, config.n_embd),
             h = nn.ModuleList([Block() for _ in range(config.n_layers)]),
             ln = nn.LayerNorm(config.n_embd)
         ))
-        self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias = False)
-        self.transformer.wte.weight = self.lm_head.weight
+        self.lm_head = nn.Linear(config.n_embd, config.stop_vocab_size, bias = False)
 
         self.apply(self._init_weight)
 
@@ -148,7 +174,7 @@ class GPT(nn.Module):
         
     def forward(self, x, targets = None):
         B, T = x.shape
-        
+
         pos = torch.arange(0, T, dtype = torch.long, device = config.device)
         tok_emb = self.transformer.wte(x)
         pos_emb = self.transformer.wpe(pos)
@@ -164,18 +190,22 @@ class GPT(nn.Module):
         if targets is not None:
             logits = logits.view(-1, logits.shape[-1])
             targets = targets.view(-1)
-            loss = F.cross_entropy(logits, targets)
+            loss = F.cross_entropy(logits, targets, ignore_index = -1)
         return logits, loss
     
-    def generate(self, ix, max_new_tokens):
-        for _ in range(max_new_tokens):
-            ix_cond = ix[:, -config.block_size:]
-            logits, loss = self(ix_cond)
+    def generate(self, ix):
+        mask_count = 0
+        length = 4#ix.shape[1]
+        while True:
+            logits, loss = self(ix)
             logits = logits[:, -1, :]
             p_dis = F.softmax(logits, dim = -1)
             next_ix = torch.multinomial(p_dis, num_samples = 1)
             ix = torch.cat((ix, next_ix), dim = -1)
-        return ix
+            if next_ix == 0:
+                mask_count += 1
+            if mask_count == 4:
+                return ix[0][length:]
         
 
     
@@ -270,46 +300,48 @@ print(x[:50], y[:x_ixs[49]+1])'''
 words_vocab = set(x)
 stop_words_vocab = stop_words.copy()
 config.vocab_size = len(words_vocab)
-config.stop_vocab_size = len(stop_words_vocab) + 1                                        # +1 for the mask token
 
 
 stoi = {word : i for i, word in enumerate(words_vocab)}
 itos = {i : word for i, word in enumerate(words_vocab)}
-stop_stoi = {word : i for i, word in enumerate(stop_words_vocab)}
-stop_itos = {i : word for i, word in enumerate(stop_words_vocab)}
+stop_xstoi = {word : i+3378 for i, word in enumerate(stop_words_vocab)}
+stop_ystoi = {word : i+1 for i, word in enumerate(stop_words_vocab)}        # +1 for the mask token
+stop_itos = {i+1 : word for i, word in enumerate(stop_words_vocab)}
+stop_itos[config.y_mask_token] = '<mask>'
+
+config.stop_emb_size = max(list(stop_xstoi.values()))+2                     # +2 length of the stop_xstoi, x_pad token
+config.stop_vocab_size = len(stop_itos)                                  
 
 encode = lambda ls: [stoi[word] for word in ls]
-stop_encode = lambda ls: [stop_stoi[word] if word in stop_stoi else config.mask_token for word in ls]
-decode = lambda ls: ' '.join([itos[ix] for ix in ls])
-stop_decode = lambda ls: ' '.join([stop_itos[ix] if ix in stop_itos else '<mask>' for ix in ls])
+stop_xencode = lambda ls: [stop_xstoi[word] if word in stop_xstoi else config.x_mask_token for word in ls]
+stop_yencode = lambda ls: [stop_ystoi[word] if word in stop_ystoi else config.y_mask_token for word in ls]
+decode = lambda ls: ''.join([itos[ix] for ix in ls])
+stop_decode = lambda ls, context: ''.join([stop_itos[ix] if ix != 0 else context.pop(0) for ix in ls])
 
-'''with open('data/words_vocab.txt', 'w') as f:
-    f.write(str(stoi))'''
     
 encoded_x = encode(x)
-encoded_y = stop_encode(y)                                  
+encoded_xy = stop_xencode(y) 
+encoded_yy =  stop_yencode(y)       
+
 encoded_x = torch.tensor(encoded_x, dtype = torch.long)
-encoded_y = torch.tensor(encoded_y, dtype = torch.long)
+encoded_xy = torch.tensor(encoded_xy, dtype = torch.long)
+encoded_yy = torch.tensor(encoded_yy, dtype = torch.long)
 
 
 n1 = int(0.9*len(encoded_x))
 train_data = encoded_x[:n1]
 val_data = encoded_x[n1:]
 
-n2 = x_ixs[len(train_data)]
-y_train_data = encoded_y[:n2]
-y_val_data = encoded_y[n2:]
+train_ixs = x_ixs[:n1]
+val_ixs = x_ixs[n1:]
 
-print("Train data size: ", len(y_train_data), "Validation data size: ", len(y_val_data))
-#print(decode(train_data[15:19].tolist()), y_train_data[x_ixs[15] : x_ixs[18]+1], stop_decode(y_train_data[x_ixs[15] : x_ixs[18]+1].tolist()))
+#print("Train data size: ", len(train_data), "Validation data size: ", len(val_data))
+#print(decode(val_data[155:162].tolist()), encoded_xy[val_ixs[155] : val_ixs[161]+1], encoded_yy[val_ixs[155] : val_ixs[161]+1], stop_decode(encoded_yy[val_ixs[155] : val_ixs[161]+1].tolist()))
 
-
-
-'''# Model initialization
+# Model initialization
 print("-"*80)
 print("Model initializing...")
-print("-"*80)  
-config = Config()   
+print("-"*80)    
 model = GPT()
 model= model.to(config.device)
 print(f"Total parameters : {sum([p.nelement()for p in model.parameters()])} parameters\t{sum([p.nelement()for p in model.parameters()]) / 1e6:.2f}M parameters\n")
@@ -337,5 +369,11 @@ print()
 
 
 #Inference
-context = torch.full((1, 1), 63, device = config.device, dtype = torch.long)
-print(f"Model response(After training)\n{decode(model.generate(context, 50).tolist()[0])}")'''
+for j in range(3):
+    i = torch.randint(0, len(val_data)-4, (1, ), dtype = torch.long, device = config.device)
+    context = val_data[i:i+4].unsqueeze(0)
+    words = [decode([k]) for k in context[0].tolist()]
+    out = model.generate(context).tolist()
+    out = stop_decode(out, words)
+    print(f"input:\n{decode(context[0].tolist())}\n\noutput:\n{out}\n")
+
